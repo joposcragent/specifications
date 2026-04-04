d769c3e230fcd2a9c6242b099cc553038f34665a
Лев Садовский <leo@sadovskie.ru>
2026-04-04 21:37:35 +0300

    заменил избыточные круды на один settings-manager

---
+++ database-schema/settings/V002_create_table_search_queries.sql
@@ -0,0 +1,13 @@
+create table settings.search_queries
+(
+    uuid       uuid                     default uuid_generate_v4() not null
+        constraint search_queries_pk
+            primary key,
+    query      varchar                                             not null,
+    created_at timestamp with time zone default now()              not null,
+    updated_at timestamp with time zone
+);
+
+alter table settings.search_queries
+    owner to postgres;
+
--- database-schema/settings/V003_create_table_reference_context.sql
+++ database-schema/settings/V003_create_table_reference_context.sql
@@ -1,13 +1,14 @@
-create table if not exists user_profile.reference_context
+create table settings.reference_context
 (
-    uuid       uuid                     default uuid_generate_v4() not null,
+    uuid       uuid                     default uuid_generate_v4() not null
+        constraint reference_context_pk
+            primary key,
     text       varchar                                             not null,
     vector     real[]                                              not null,
     created_at timestamp with time zone default now()              not null,
     updated_at timestamp with time zone
 );
 
-alter table user_profile.reference_context
-    add constraint reference_context_pk
-        primary key (uuid);
+alter table settings.reference_context
+    owner to postgres;
 
