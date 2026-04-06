8811b189202d8afe1a130e3c67761637883d1991
Лев Садовский <leo@sadovskie.ru>
2026-04-06 12:11:01 +0300

    опечатка в названии сервиса + убил ненужный uuid

--- database-schema/settings/V5__create_table_css_query_selectors.sql
+++ database-schema/settings/V5__create_table_css_query_selectors.sql
@@ -1,17 +1,12 @@
 create table settings.css_query_selectors
 (
-    uuid       uuid                     default gen_random_uuid() not null
+    type       settings.css_query_selector_type       not null
         constraint css_query_selectors_pk
             primary key,
-    selector   varchar                                             not null,
-    type       settings.css_query_selector_type                    not null
-        constraint css_query_selectors_uk_type
-            unique,
-    created_at timestamp with time zone default now()              not null,
+    selector   varchar                                not null,
+    created_at timestamp with time zone default now() not null,
     updated_at timestamp with time zone
 );
-
-comment on constraint css_query_selectors_uk_type on settings.css_query_selectors is 'Не может быть больше одного селектора одного типа';
 
 alter table settings.css_query_selectors
     owner to postgres;
