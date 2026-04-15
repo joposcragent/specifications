d7863f9c8f512ec2247a2aeb16b3a97438851a6f
Лев Садовский <leo@sadovskie.ru>
2026-04-15 13:14:33 +0300

    85 and 92 default thresholds

---
+++ database-schema/settings/V6__default_thresholds.sql
@@ -0,0 +1,5 @@
+INSERT INTO settings.relevance_thresholds (type, value, created_at)
+VALUES ('CONTENT'::settings.threshold_type, 85, DEFAULT)
+
+INSERT INTO settings.relevance_thresholds (type, value, created_at, updated_at)
+VALUES ('NOTIFICATION'::settings.threshold_type, 92, DEFAULT)
